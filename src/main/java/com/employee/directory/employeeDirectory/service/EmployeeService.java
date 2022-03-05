2e2f1179f9591ffd59307348aab94d31cc469283
Hanine <haniiny2010ma@gmail.com>
2022-03-05 12:08:25 +0100

    Add Employee List

---
+++ src/main/java/com/employee/directory/employeeDirectory/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.employee.directory.employeeDirectory.service;
+
+import com.employee.directory.employeeDirectory.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+}
